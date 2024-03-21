51dc233b2003d071112a3ad9eb4c9da535318d53
William Davidson <wj.davidson@outlook.com>
2024-03-21 11:50:56 +0100

    removed userContext from Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import useUserContext from "../hooks/useUserContext";
 
 export default function Home() {
-  const { user } = useUserContext();
-
   return (
     <div>
       <h1>LIA-Event</h1>
-      <p>{user.name}</p>
     </div>
   );
 }
