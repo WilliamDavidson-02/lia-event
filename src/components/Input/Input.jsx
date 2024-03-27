dc8720c618aa3948ffcdae1e9def4081c0770841
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-03-27 13:33:46 +0100

    login work

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-export default function InputEmail({ type, labelText, placeholderText }) {
+export default function InputEmail({ type, labelText, placeholderText, ...props }) {
   return (
     <label className={styles.container}>
       <p>{labelText}</p>
-      <input className={styles.inputField} type={type} placeholder={placeholderText} />
+      <input className={styles.inputField} type={type} placeholder={placeholderText} {...props} />
     </label>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,26 +4,21 @@ import Nav from "../../components/Nav/Nav";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 import CountDown from "../../components/CountDown/CountDown";
-import Login from "../../components/Login/Login";
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      {/* <Nav />
+      <Nav />
       <section className={styles.hero}>
         <CountDown target={"Apr 23, 2024, 15:00:00"} />
         <div className={styles.content}>
           <h1>Welcome to our industry meetup!</h1>
-          <p>
-            Meet students from webdeveloper- and digital designer courses at
-            YRGO.
-          </p>
+          <p>Meet students from webdeveloper- and digital designer courses at YRGO.</p>
           <Link to={"/onboarding"}>
             <Button>Atend event</Button>
           </Link>
         </div>
-      </section> */}
-      <Login />
+      </section>
     </main>
   );
 }
