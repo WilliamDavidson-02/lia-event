7a2282446f8316b43cd8a86049bc57d66827c40e
William Davidson <wj.davidson@outlook.com>
2024-03-29 17:06:51 +0100

    removed  onboarding card and moved styling to onboarding

--- src/components/OnboardingCard/OnboardingCard.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./OnboardingCard.module.css";
-
-function OnboardingCard({ children }) {
-  return <div className={styles.container}>{children}</div>;
-}
-
-export default OnboardingCard;
