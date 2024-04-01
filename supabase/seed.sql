5ee997c20d64ee67e7cbe395999673c3fc3151b2
William Davidson <wj.davidson@outlook.com>
2024-04-01 12:13:56 +0200

    added auth.users seed

---
+++ supabase/seed.sql
@@ -0,0 +1,40 @@
+INSERT INTO
+auth.users (
+    instance_id,
+    id,
+    aud,
+    role,
+    email,
+    encrypted_password,
+    email_confirmed_at,
+    recovery_sent_at,
+    last_sign_in_at,
+    raw_app_meta_data,
+    raw_user_meta_data,
+    created_at,
+    updated_at,
+    confirmation_token,
+    email_change,
+    email_change_token_new,
+    recovery_token
+) (
+select
+    '00000000-0000-0000-0000-000000000000',
+    uuid_generate_v4 (),
+    'authenticated',
+    'authenticated',
+    'user' || (ROW_NUMBER() OVER ()) || '@example.com',
+    crypt ('password123', gen_salt ('bf')),
+    current_timestamp,
+    current_timestamp,
+    current_timestamp,
+    '{"provider":"email","providers":["email"]}',
+    '{"area":["developer","design"],"contact":"company@gmail.com","employees":"101 to 500","keywords":["Frontend","Backend","CSS","SCSS","Typescript","JavaScript","Figma","React","Headless"],"name":"company","userType":"company"}',
+    current_timestamp,
+    current_timestamp,
+    '',
+    '',
+    '',
+    ''
+FROM generate_series(1, 100)
+);
