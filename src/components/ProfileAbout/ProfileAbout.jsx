e075597147a9fe484719ec6e53f1cc75496caba2
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-04-05 14:06:13 +0200

    public profile work

--- src/components/ProfileAbout/ProfileAbout.jsx
+++ src/components/ProfileAbout/ProfileAbout.jsx
@@ -1,24 +1,60 @@
+import React from "react";
 import Tab from "../Tab/Tab";
 import Tabs from "../Tabs/Tabs";
 import styles from "./ProfileAbout.module.css";
+import Chips from "../Chips/Chips";
 
-export default function ProfileAbout({ sectionHeader }) {
+export default function ProfileAbout({ userType, userData }) {
   return (
     <div className={styles.container}>
-      <Tabs style={{ height: "48.125rem" }}>
-        <Tab>
-          <h3>Om oss</h3>
-          <p>Branchroll</p>
-          <p>Antal Anställda:</p>
-          <p>Onsite/ Remote/ Hybrid</p>
-        </Tab>
-        <Tab>
-          <h3>{sectionHeader}</h3>
-        </Tab>
-        <Tab>
-          <h3>{sectionHeader}</h3>
-        </Tab>
-      </Tabs>
+      {userType === "company" && (
+        <Tabs style={{ height: "48.125rem" }}>
+          <Tab>
+            <h3>About Us</h3>
+            <p>
+              Field:{" "}
+              {userData.area.map((areaTitle, index) => (
+                <React.Fragment key={index}>
+                  {areaTitle}
+                  {/* If a company has both web devs and design, adds both separated by a forward slash */}
+                  {index !== userData.area.length - 1 && "/"}
+                </React.Fragment>
+              ))}
+            </p>
+          </Tab>
+          <Tab>
+            <h3>What We're Looking For</h3>
+            <Chips defaultValue={userData.profile.keywords} />
+            <p>Fri text</p>
+          </Tab>
+          <Tab>
+            <h3>Contact</h3>
+            <p>Mail: {userData.contact}</p>
+            <p>Phone:</p>
+          </Tab>
+        </Tabs>
+      )}
+
+      {userType === "student" && (
+        <Tabs style={{ height: "48.125rem" }}>
+          <Tab>
+            <h3>Om mig</h3>
+            <p>Utbildning (pågående):</p>
+            <p>Location:</p>
+          </Tab>
+          <Tab>
+            <h3>Jag är</h3>
+            <p>Chips här</p>
+            <p>Dev/ Design</p>
+            <p>Fri text</p>
+          </Tab>
+          <Tab>
+            <h3>Kontakt</h3>
+            <p>Mail:</p>
+            <p>Tel:</p>
+          </Tab>
+        </Tabs>
+      )}
     </div>
   );
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
+import useUserContext from "../../hooks/useUserContext";
+import { useParams } from "react-router-dom";
+
+import supabase from "../../config/supabaseConfig";
+
 import styles from "./Profile.module.css";
 import BusinessCard from "../../components/BusinessCard/BusinessCard";
 import ProfileAbout from "../../components/ProfileAbout/ProfileAbout";
+import UserCard from "../../components/UserCard/UserCard";
 
 export default function Profile() {
+  const [userData, setUserData] = useState(null);
+  const { userID, userType } = useParams();
+  const { user } = useUserContext;
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const { data, error } = await supabase
+        .from("company_profile")
+        .select("*, profile(*)")
+        .eq("id", userID)
+        .single();
+
+      if (error) {
+        console.log("error fetching profile", error);
+        return;
+      }
+      console.log(data);
+      setUserData(data);
+    }
+
+    fetchUserData();
+  }, [userID]);
   return (
     <div className={styles.container}>
-      <BusinessCard name="Hello" url="google.se" profileImg="#" />
-      <ProfileAbout sectionHeader="Test" />
+      {userData && (
+        <>
+          <BusinessCard name={userData.profile.name} url={userData.href || "url not set"} profileImg="#" />
+          {/* <UserCard /> */}
+          <ProfileAbout userType={userType} userData={userData} />
+        </>
+      )}
     </div>
   );
 }
